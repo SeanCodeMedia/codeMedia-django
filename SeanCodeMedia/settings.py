3f3c1fb6ee97a403b24a0289dc71e87ffe0bf53c
Sean D Peart <seandp32@gmail.com>
2022-04-18 22:30:45 -0400

    Update settings.py

--- SeanCodeMedia/settings.py
+++ SeanCodeMedia/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 # START
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 # MEDIA_URL = '/media/' 
-MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
 
  # uncomment if you want to host static files from spaces 
  # https://www.digitalocean.com/community/questions/how-to-store-django-media-files-to-spaces
